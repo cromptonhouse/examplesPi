fd288bffd446db119fa248137943d6c5d016414b
Ewan <ewan.cook@live.com>
2014-06-05 11:34:51 +0100

    Improved slightly - fixed issues + cleaned up

--- findflag.py
+++ findflag.py
@@ -1,29 +1,23 @@
 # C.Raynerd 11.2.14 - Find the flag
-# Version 1.0 of the code - basic
-
+# Version 1.1 of the code - improved slightly
+import random
 print "Welcome to find the flag!"
 print "There are, 50 doors and you have to find the flag in the least moves" 
 
 count = 0          #somewhere to count the number of moves
-flagDoor = 17
+flagDoor = random.randint(1, 50)
 
 while 1:
-   print "Please pick a door..."
-   count = count + 1                     #add one to the number of moves
-   currentDoor = raw_input(">")          #input a door number  
-   
-   if int(currentDoor) == flagDoor:
-       print "You found the flag in", count,"moves" 
-       break
-
-   else:
-        
+    count += 1                    #add one to the number of moves
+    currentDoor = raw_input("Please pick a door... > ")          #input a door number  
+    if int(currentDoor) == flagDoor:
+        print "You found the flag in", count,"moves" 
+        break
+    else:
         distance = int(currentDoor) - flagDoor   # calculates the distance away
         if -4 < distance < 4:
-		print "You are close!!"
-	else:
-		print "Miles away" 
-	
+            print "You are close!!"
+        else:
+            print "Miles away"
 
-   
 
